0088c596d79dc4d035591ed10ecdfab1705a4490
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-10-24 10:02:34 -0500

    fixed employee/supervisor constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023175104) do
+ActiveRecord::Schema.define(version: 20171023212024) do
 
   create_table "computers", force: :cascade do |t|
     t.date "commission_date"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20171023175104) do
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.boolean "status"
+    t.boolean "supervisor_status"
     t.integer "department_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
