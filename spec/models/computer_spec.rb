7262c126e56b36e0c0cf0da3614ed15ff836e476
beelarr <bryonl@me.com>
2017-10-25 09:13:32 -0500

    Tinkering with testing

--- spec/models/computer_spec.rb
+++ spec/models/computer_spec.rb
@@ -1,14 +1,16 @@
-RSpec.describe 'Computer' do
-  it { is_expected.to have_db_column :commission_date }
+# RSpec.describe 'Computer' do
+#   it { is_expected.to have_db_column :commission_date }
+#
+#   context 'Computer commission confirmation. . .' do
+#     it 'activates a Computer' do
+#     expect{Computer.commission_date}.to eq(Computer.commission_date)
+#     end
+#   end
+# end
+require 'spec_helper'
 
-  context 'Computer commission confirmation. . .' do
-    it 'activates a Computer' do
-    expect{Computer.commission_date}.to eq(Computer.commission_date)
-    end
+RSpec.describe 'Computer' :type => :model do
+  it "is not valid without a commission_date" do
+    expect(Computer.new).to be_valid
   end
-end
-
-
-RSpec.describe 'Computer' do
-  it { is_expected.to include(:id) }
 end
